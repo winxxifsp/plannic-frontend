eb85d4e4218f0ba8f5fedd64bc8a95b9bc8730e3
DeborAguiar <50847540+DeborAguiar@users.noreply.github.com>
2021-01-30 12:23:06 -0300

    Pagina inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { IndexComponent } from './pages/index/index.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IndexComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
