f2454269a7477648ef276cdbc3cfab2fe459a35a
Vitor Massao Sugai <massao.sg@gmail.com>
2021-01-31 14:22:22 -0300

    enviroment update

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -2,11 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { environmentProd } from 'src/environments/environment.prod';
 
 
-//const API_URL = `${environment.API_URL}`;
-const API_URL = `${environmentProd.API_URL}`;
+const API_URL = `${environment.API_URL}`;
 
 @Injectable({
   providedIn: 'root'
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-export const environmentProd = {
+export const environment = {
   production: true,
   API_URL: 'https://plannic-back.herokuapp.com/'
 };
